210f5ac58f2ce02d903a698ce3e99fb623296ac0
HueyDinh <86534851+HueyDinh@users.noreply.github.com>
2021-06-26 16:06:46 -0400

    initial Commit

---
+++ truss_main.m
@@ -0,0 +1,6 @@
+node_data = readmatrix("node_data.csv");
+unkown_list = readmatrix("unknown_list.csv");
+f_x = node_data(:,4);
+f_y = node_data(:,5);
+f_xy = [f_x,f_y]';
+lin_force_v = reshape(f_xy,[],1);
