deb78ddefb9b92247b93f58d54e4c0d9c75a2229
Hieu Dinh <86534851+HueyDinh@users.noreply.github.com>
2021-06-26 22:51:18 -0400

    Fixed the algorithm for detecting function fizzling out. Scripts seems to be working

--- MEM 255 Controls/HW1/mem255_hw1.m
+++ MEM 255 Controls/HW1/mem255_hw1.m
@@ -1,28 +1,42 @@
 gamma_0=0.25;
-gamma_list =linspace(0,2,10000);
+gamma_list =linspace(0.3,1.9,1000);
 init = [2 0]';
-threshold =init(1)*0.0001;
-t_span = [0 100];
+y_thres =init(1)*0.001;
+dy_thres = 0.002;
+t_span = [0 25/gamma_0];
 [t,y] = ode45(@(t,y) p1(t,y,gamma_0),t_span,init);
-tau = negligible(t,y(:,1),threshold);
-% temp_funct = @(gamma) tau_gamma_pair(@p1,t_span,init,gamma,threshold);
-% [tau_series,gamma_series] = arrayfun(temp_funct,gamma_list);
+tau = negligible(t,y,y_thres,dy_thres);
+temp_funct = @(gamma) tau_gamma_pair(@p1,init,gamma,y_thres,dy_thres);
+[tau_series,gamma_series] = arrayfun(temp_funct,gamma_list);
+tau_a = arrayfun(@analytic,gamma_list);
 
-f1=figure;
+f1=figure("name","Differential equation Solution gamma=0.25");
 plot1 = plot(t,y(:,1),'r');
-axis tight;
+title('Initial Value Problem Solution with \gamma =0.25')
+xlabel('t')
+ylabel('y')
+
+f2=figure("name","Analytic solution vs. Estimation Algorithm");
+plot2 = plot(gamma_series,tau_series,'r--',gamma_list,tau_a);
+title('Time Until Solution Fade Out (\tau) vs. \gamma')
+xlabel('\gamma')
+ylabel('\tau')
 
 function dy = p1(t,y,gamma)
 dy = [y(2);-gamma*y(2)-y(1)];
 end
 
-function t = negligible(t,y,threshold)
-index = abs(y) < threshold;
+function t = negligible(t,y,y_threshold,dy_threshold)
+index = (abs(y(:,1)) < y_threshold) & (abs(y(:,2)) < dy_threshold);
 t_list=t(index);
 t=t_list(1);
 end
 
-function [tau,gamma] = tau_gamma_pair(handle,t_span,init,gamma,threshold)
-[t,y] = ode45(@(t,y) handle(t,y,gamma),t_span,init);
-tau = negligible(t,y(:,1),threshold);
+function [tau,gamma] = tau_gamma_pair(handle,init,gamma,y_thres,dy_thres)
+[t,y] = ode45(@(t,y) handle(t,y,gamma),[0 40/gamma],init);
+tau = negligible(t,y,y_thres,dy_thres);
+end
+
+function tau = analytic(gam)
+tau=-2/gam*log(0.002*(4-gam^2)^0.5/4);
 end
